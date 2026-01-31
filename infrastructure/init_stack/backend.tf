286b09573c655930f6a4ffac0438c8ee9ae36eb4
Radhi Hudijan <radhi.hudijan@accenture.com>
2026-01-31 17:11:56 +0100

    fix: correct formatting of backend configuration in backend.tf

--- infrastructure/init_stack/backend.tf
+++ infrastructure/init_stack/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "qr-generator-devops-terraform-state-backend-dev"
-    key            = "terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "qr-generator-devops-terraform-locks"
+    bucket = "qr-generator-devops-terraform-state-backend-dev"
+    key    = "terraform.tfstate"
+    region = "eu-central-1"
+
 
   }
 }
